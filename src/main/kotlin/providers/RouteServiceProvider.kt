ecd7c09fd37854b5470e8828b91dba9073a42631
Ashok Gelal <ashokgelal@gmail.com>
2019-11-19 22:47:41 -0700

    Upgrade to the latest Alpas version

--- src/main/kotlin/providers/RouteServiceProvider.kt
+++ src/main/kotlin/providers/RouteServiceProvider.kt
@@ -1,14 +1,11 @@
 package dev.alpas.alpasdev.providers
 
-import dev.alpas.Application
 import dev.alpas.alpasdev.addRoutes
-import dev.alpas.make
 import dev.alpas.routing.RouteServiceProvider
 import dev.alpas.routing.Router
 
 class RouteServiceProvider : RouteServiceProvider() {
-    override fun register(app: Application) {
-        super.register(app)
-        app.make<Router>().addRoutes()
+    override fun loadRoutes(router: Router) {
+        router.addRoutes()
     }
 }
