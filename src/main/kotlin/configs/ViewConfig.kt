1c020ded6a65380d7770dc0524155304b80ce774
Ashok Gelal <ashokgelal@gmail.com>
2019-12-14 10:40:50 -0700

    Upgrade to Alpas 0.5

--- src/main/kotlin/configs/ViewConfig.kt
+++
@@ -1,9 +0,0 @@
-package dev.alpas.alpasdev.configs
-
-import dev.alpas.Environment
-import dev.alpas.view.ViewConfig
-
-@Suppress("unused")
-class ViewConfig(env: Environment) : ViewConfig(env) {
-    override val templateExtension = "peb"
-}
