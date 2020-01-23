2c18c7e192f50ac7e03b21c10e5944dddd744a8e
Ashok Gelal <ashokgelal@gmail.com>
2020-01-23 12:38:31 -0700

    Add nav links

---
+++ src/main/kotlin/configs/LinksConfig.kt
@@ -0,0 +1,9 @@
+package dev.alpas.alpasdev.configs
+
+import dev.alpas.Config
+
+class LinksConfig : Config {
+    val twitter = "https://twitter.com/alpasdev"
+    val slack = "https://join.slack.com/t/alpasdev/shared_invite/enQtODcwMjE1MzMxODQ3LTJjZWMzOWE5MzBlYzIzMWQ2MTcxN2M2YjU3MTQ5ZDE4NjBmYjY1YTljOGIwYmJmYWFlYjc4YTcwMDFmZDIzNDE"
+    val github = "https://github.com/alpas/alpasdev"
+}
