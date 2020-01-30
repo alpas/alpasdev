be263ae9b138cb53df4e90c118d0c65f48b9bbb3
Adam Miedema <amiedema@metageek.net>
2020-01-30 10:07:34 -0700

    newsletter signup

--- src/main/kotlin/routes.kt
+++ src/main/kotlin/routes.kt
@@ -2,6 +2,8 @@ package dev.alpas.alpasdev
 
 import dev.alpas.alpasdev.controllers.DocsController
 import dev.alpas.alpasdev.controllers.HomeController
+import dev.alpas.alpasdev.controllers.NewsletterController
+import dev.alpas.alpasdev.controllers.ThanksController
 import dev.alpas.routing.Router
 
 fun Router.addRoutes() = apply {
@@ -12,4 +14,6 @@ private fun Router.webRoutes() {
     get("/", HomeController::class).name("welcome")
     get("/docs/<page>", DocsController::class, "show").name("docs.show")
     get("/docs", DocsController::class, "index").name("docs.index")
+    get("/newsletter", NewsletterController::class, "index").name("newsletter")
+    get("/thankyou", ThanksController::class, "index").name("thankyou")
 }
